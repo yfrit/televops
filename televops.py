122602fc729cfe274918c8f09baae3de312f49d1
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2021-04-02 23:50:20 -0300

    Improved task tree display structure
    - Removed trailing period
    - Included parent story
    - Included nice ascii art

--- televops.py
+++ televops.py
@@ -1,22 +1,19 @@
 import logging
 import os
-import telegram.ext
-
-from telegram.ext import CommandHandler
-from telegram.ext import Updater
-from devops_client import Client
-from devops_client import Task
 from datetime import datetime
-from datetime import timezone
+
+import telegram.ext
 from dotenv import load_dotenv
+from telegram.ext import CommandHandler, Updater
+
+from devops_client import Client
 
 # load env file
 load_dotenv()
 
 # fetch updater and job queue
 print("Starting bot...")
-updater = Updater(token=os.getenv("TELEGRAM_TOKEN"),
-                  use_context=True)
+updater = Updater(token=os.getenv("TELEGRAM_TOKEN"), use_context=True)
 dispatcher = updater.dispatcher
 
 # set the logging stuff
@@ -33,27 +30,34 @@ client = Client(colaborators)
 def daily(update, context):
     heading = f'{datetime.now()}\n'
     heading += "Welcome to today's Yfrt's Televops Daily Meeting!\n\n"
-    heading += 'Here are the current statuses:\n\n'
 
     body = ""
     tasks = client.get_tasks(include_completed=True)
-    
+
     for owner, tasks in tasks.items():
         body += owner + ' is working on:\n'
         body += '```\n'
+
         if tasks:
+            last_parent = 0
             for task in tasks:
-                body += f"{task.id}. {task.name} ({task.state})."
+                parent = task.parent
+                if last_parent != parent.id:
+                    last_parent = parent.id
+                    body += f"├── {parent.id}. {parent.name} - ({parent.state})"  # noqa
+                    body += "\n"
+
+                body += f"│   ├── {task.id}. {task.name} - ({task.state})"
                 body += "\n"
         else:
             body += 'No tasks in progress.\n'
         body += '```' + '\n'
 
-    body += "Please, type in your current status. Don't forget to include what you're doing, what you plan to do, and if you have any impediments!"
+    body += "Please, type in your current status. Don't forget to include what you're doing, what you plan to do, and if you have any impediments!"  # noqa
 
     msg = str(heading + body).replace('-', '\-') \
                              .replace('.', '\.') \
-                             .replace('!', '\!')
+                             .replace('!', '\!') # noqa
 
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=msg,

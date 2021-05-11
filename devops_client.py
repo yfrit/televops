1b443ad702bdd53bffd6e10a303ea17c5659a240
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2021-05-11 00:31:09 -0300

    Added more stats to daily header
    - Added scope size and projected date
    - Changed completed percentage to float
    - Improved readabililty on header
    - Implemented TaskBuilder to improve code readibility

--- devops_client.py
+++ devops_client.py
@@ -1,8 +1,10 @@
 import asyncio
 import datetime
+import math
 import os
 
 from azure.devops.connection import Connection
+from azure.devops.v5_1.work.models import TeamContext
 from azure.devops.v5_1.work_item_tracking.models import Wiql
 from dotenv import load_dotenv
 from msrest.authentication import BasicAuthentication
@@ -29,6 +31,16 @@ class Task:
         return self.__str__()
 
 
+class TaskBuilder():
+    def from_work_item(self, work_item):
+        wid = work_item.id
+        name = work_item.fields['System.Title']
+        owner = work_item.fields['System.AssignedTo']['displayName']
+        state = work_item.fields['System.State']
+
+        return Task(id=wid, name=name, owner=owner, state=state)
+
+
 class Client:
     def __init__(self, collaborators):
         self._colaborators = collaborators
@@ -41,7 +53,10 @@ class Client:
         credentials = BasicAuthentication('', personal_access_token)
         connection = Connection(base_url=organization_url, creds=credentials)
 
+        # clients and controllers
         self._wit_client = connection.clients.get_work_item_tracking_client()
+        self._task_builder = TaskBuilder()
+        self._work_client = connection.clients.get_work_client()
 
     def _query_by_wiql(self, query, top=100):
         wiql = Wiql(query=query)
@@ -64,7 +79,6 @@ class Client:
                 AND (
                         [Target].[System.Id] = {task_id}
                 )
-        ORDER BY [System.Id]
         MODE (Recursive, ReturnMatchingChildren)
         """ # noqa
         wiql_results = self._query_by_wiql(query, top=30).work_item_relations
@@ -105,19 +119,11 @@ class Client:
                           for res in wiql_results)
             for work_item in work_items:
                 # create child
-                wid = work_item.id
-                name = work_item.fields['System.Title']
-                owner = work_item.fields['System.AssignedTo']['displayName']
-                state = work_item.fields['System.State']
-                child = Task(id=wid, name=name, owner=owner, state=state)
+                child = self._task_builder.from_work_item(work_item)
 
                 # get parent
-                story_item = self._get_parent_by_task_id(wid)
-                wid = story_item.id
-                name = story_item.fields['System.Title']
-                owner = story_item.fields['System.AssignedTo']['displayName']
-                state = story_item.fields['System.State']
-                parent = Task(id=wid, name=name, owner=owner, state=state)
+                story_item = self._get_parent_by_task_id(child.id)
+                parent = self._task_builder.from_work_item(story_item)
 
                 child.parent = parent
                 results.append(child)
@@ -140,12 +146,55 @@ class Client:
         not_done_results = list(self._wit_client.query_by_id(qid).work_items)
         not_done_count = len(not_done_results)
 
-        # TODO: use the objects for cool things, for now, too lazy
-        # maye completion dates to plot estimated project delivery
+        # get sprint start date to make comparisons
+        team_context = TeamContext(project='Card Game', team='Card Game Team')
+        iteration = self._work_client.get_team_iterations(
+            team_context, timeframe='current')[0]
+        first_date = iteration.attributes.start_date.date()
+        release_date = iteration.attributes.finish_date.date()
+
+        # get completion dates and match them with sprint start date
+        created_dates = []
+        work_items = (self._wit_client.get_work_item(int(res.id))
+                      for res in done_results)
+        for work_item in work_items:
+            # convert to datetime object
+            closed_date_str = work_item.fields[
+                'Microsoft.VSTS.Common.ClosedDate']
+            closed_date = datetime.datetime.strptime(
+                closed_date_str, '%Y-%m-%dT%H:%M:%S.%fZ').date()
+            closed_date_str = str(closed_date)
+
+            # store created dates
+            created_date = datetime.datetime.strptime(
+                work_item.fields['System.CreatedDate'],
+                '%Y-%m-%dT%H:%M:%S.%fZ').date()
+            created_dates.append(str(created_date))
+
+        # get number of worked days
+        today = datetime.datetime.now().date()
+        total_days = (today - first_date).days
+
+        # get average
+        average = done_count / total_days
+
+        # calculate projected date
+        days_left = not_done_count / average
+        projected_date = today + datetime.timedelta(days=math.ceil(days_left))
+
+        # calculate increased scope
+        increased_scope = 0
+        for created_date in created_dates:
+            if created_date != str(first_date):
+                increased_scope += 1
+
         results = {
             'done': done_count,
             'total': done_count + not_done_count,
-            'completed': done_count / (done_count + not_done_count)
+            'completed': done_count / (done_count + not_done_count),
+            'projected_date': projected_date,
+            'release_date': release_date,
+            'increased_scope': increased_scope
         }
 
         return results
